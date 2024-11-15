1f3f919385a219ca26ab8a8a6d2c45f73c183c61
Patrick Zhao <patrick.zhao1989@outlook.com>
2024-11-15 23:20:12 +1100

    update code

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,11 +6,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
+if (app.Environment.IsDevelopment()){
+    app.UseCors("AllowAll");
+}
 
 
 app.UseHttpsRedirection();
--- frontend/azurehackday-frontend/src/App.tsx
+++ frontend/azurehackday-frontend/src/App.tsx
@@ -1,10 +1,32 @@
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import axiosInstance from './api/axiosInstance';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    // Replace with your backend API URL
+    axiosInstance
+      .get("/HelloWorld")
+      .then((response) => {
+        setData(response.data); // Handle the data from the response
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message); // Handle errors
+        setLoading(false);
+      });
+  }, []); // Empty array ensures this runs only once after the component mounts
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
   return (
     <>
         <h1>Hello World Everyone Today is a good Day huh?</h1>
+        <h1>{data}</h1>
     </>
   )
 }
